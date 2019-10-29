8063cf965a7762e9125cfb4913b46e8592e4c295
oymdev04 <oymdev4@oym.com.py>
2019-10-29 16:33:03 -0300

    Agrega servicio para comprar

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -3,6 +3,8 @@ import { ActivatedRoute } from '@angular/router';
 // importar el archivo que tiene los datos del producto desde un archivo externo
 
 import { products } from '../products';
+// se importa el servico del carrito
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-product-details',
@@ -15,15 +17,22 @@ export class ProductDetailsComponent implements OnInit {
 
   constructor(
     private route: ActivatedRoute,
+    private cartService: CartService, // se inyecta el servicio
   ) { }
-  //El ActivatedRoutees específico para cada componente enrutado cargado por el enrutador angular. Contiene información sobre la ruta, sus parámetros y datos adicionales asociados con la ruta.
+  // El ActivatedRoutees específico para cada componente enrutado cargado por el enrutador angular.
+  // Contiene información sobre la ruta, sus parámetros y datos adicionales asociados con la ruta.
 
-//angular llamada a ngOnInit poco despues de crear un componente
+// angular llamada a ngOnInit poco despues de crear un componente
 
   ngOnInit() {
     this.route.paramMap.subscribe(params => {
       this.product = products[+params.get('productId')];
-    })
+    });
+  }
+
+  addToCart(product) {
+    window.alert('Se agregó su producto al carrito!');
+    this.cartService.addToCart(product);
   }
 
 }
