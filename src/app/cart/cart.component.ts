9effb9db20b5cb098cd6e729bcc4ee5526617f37
oymdev04 <oymdev4@oym.com.py>
2019-10-31 11:59:36 -0300

    Add form para realizar la compra

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 import { CartService } from '../cart.service';
 
 
@@ -7,15 +8,43 @@ import { CartService } from '../cart.service';
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.css']
 })
-export class CartComponent implements OnInit {
+// export class CartComponent implements OnInit {
 
+//   items;
+//   checkoutForm; // para guardar el form model
+//   constructor(
+//     private cartService: CartService,
+//     private formBuilder: FormBuilder,
+//   ) { }
+
+//   ngOnInit() {
+//     this.items = this.cartService.getItems();
+//   }
+
+// }
+
+export class CartComponent {
   items;
-  constructor(
-    private cartService: CartService
-  ) { }
+  checkoutForm;
 
-  ngOnInit() {
+  constructor(
+    private cartService: CartService,
+    private formBuilder: FormBuilder,
+  ) {
     this.items = this.cartService.getItems();
+
+    this.checkoutForm = this.formBuilder.group({
+      name: '',
+      address: ''
+    });
+  }
+
+  onSubmit(customerData) {
+    // Process checkout data here
+    console.warn('Your order has been submitted', customerData);
+
+    this.items = this.cartService.clearCart();
+    this.checkoutForm.reset();
   }
 
 }
